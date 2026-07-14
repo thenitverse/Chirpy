7bf7416f9ffb41d108f2db2a7768bb770d3ad323
nitika2205 <knitika567@gmail.com>
2026-07-14 15:58:50 +0530

    Add main.go with fileserver logic

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
+
+}
