382c9f239f59f684b9ac2390433cb8681880b8bb
nitika2205 <knitika567@gmail.com>
2026-07-16 21:08:16 +0530

    added handler func to main.go.

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
