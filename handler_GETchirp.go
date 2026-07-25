8576697f19cbcbc804844f706af2b174b4c83089
nitika2205 <knitika567@gmail.com>
2026-07-25 17:55:07 +0530

    Add handlerGetChirp to fetch a single chirp by ID

--- handler_GETchirp.go
+++ handler_GETchirp.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	reqChirp, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
-		log.Printf("Error getting chirp: %s", err)
+		log.Printf("Error getting chirps: %s", err)
 		w.WriteHeader(500)
 		return
 	}
@@ -28,6 +32,43 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	data, err := json.Marshal(Chirps)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(data)
+
+}
+func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("chirpID")
+	val, err := uuid.Parse(id)
+	if err != nil {
+		log.Printf("Invalid UUID Parse: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+	onechirp, err := cfg.db.GetChirp(r.Context(), val)
+	if errors.Is(err, sql.ErrNoRows) {
+		w.WriteHeader(404)
+		return
+	}
+	if err != nil {
+		log.Printf("Error getting chirp: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	responseChirp := Chirp{
+		ID:        onechirp.ID,
+		CreatedAt: onechirp.CreatedAt,
+		UpdatedAt: onechirp.UpdatedAt,
+		Body:      onechirp.Body,
+		UserID:    onechirp.UserID,
+	}
+	data, err := json.Marshal(responseChirp)
+	if err != nil {
+		log.Printf("Error marshalling data: %s", err)
 		w.WriteHeader(500)
 		return
 	}
