2675093831f5dcb3e3af4f8b41f066968808c2a9
nitika2205 <knitika567@gmail.com>
2026-07-26 14:04:23 +0530

    add hashed passwords to users

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,9 +1,12 @@
 -- name: CreateUser :one
-INSERT INTO users (id,created_at,updated_at,email)
+INSERT INTO users (id,created_at,updated_at,email,hashed_password)
 VALUES (gen_random_uuid(),
 CURRENT_TIMESTAMP,
 CURRENT_TIMESTAMP,
-$1)
+$1,$2)
 RETURNING *;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1;
 -- name: DeleteUser :exec
 DELETE FROM users;
