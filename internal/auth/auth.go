ecf2d409dfa407020040b45b9b72ad4db6d8d208
nitika2205 <knitika567@gmail.com>
2026-07-27 20:57:23 +0530

    Add MakeJWT and ValidateJWT functions

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,6 +2,10 @@ package auth
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 
 	"github.com/alexedwards/argon2id"
 )
@@ -21,4 +25,32 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	}
 	return pass, nil
 
+}
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	key := []byte(tokenSecret)
+	tokenString, err := token.SignedString(key)
+	return tokenString, err
+
+}
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+
+	_, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	id, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return id, nil
 }
