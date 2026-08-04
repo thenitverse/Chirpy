a193347c5cb91bf8a211914a3f7a622d75390fbc
nitika2205 <knitika567@gmail.com>
2026-08-04 18:26:04 +0530

    updated  users table.

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -16,3 +16,9 @@ UPDATE users
 SET email = $1, hashed_password = $2,updated_at = CURRENT_TIMESTAMP
 WHERE id = $3
 RETURNING *;
+
+-- name: UpdateChirpy :one
+UPDATE users
+SET is_chirpy_red = true
+WHERE id = $1
+RETURNING *;
