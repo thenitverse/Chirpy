1be79c20c1358aae23f69a9c6dc211fa3a565aff
nitika2205 <knitika567@gmail.com>
2026-08-06 21:32:25 +0530

    added query for getting chirp by auther id.

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -16,3 +16,5 @@ SELECT * FROM chirps WHERE id = $1;
 
 -- name: DeleteChirp :exec
 DELETE FROM chirps WHERE id = $1 AND user_id = $2;
+-- name: GetChirpsByAuthor :many
+SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
